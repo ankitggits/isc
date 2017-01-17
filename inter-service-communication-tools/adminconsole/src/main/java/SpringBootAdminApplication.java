c5dc165149a4b0f2428159442751787de02785ab
Ankit <ankit.java8@gmail.com>
2017-01-17 01:18:01 +0100

    Removed build issues

---
+++ inter-service-communication-tools/adminconsole/src/main/java/SpringBootAdminApplication.java
@@ -0,0 +1,16 @@
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Ankit on 17-01-2017.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
