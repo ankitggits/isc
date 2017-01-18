32213931015a20c28ee4f8b1a0c40a55f9bcdd38
Ankit <ankit.java8@gmail.com>
2017-01-19 00:44:15 +0100

    Spring admin

--- inter-service-communication-test-client/eureka-server/src/main/java/no/sample/isc/testclient/euraka/server/EurekaServerApplication.java
+++ inter-service-communication-test-client/eureka-server/src/main/java/no/sample/isc/testclient/euraka/server/EurekaServerApplication.java
@@ -1,6 +1,5 @@
 package no.sample.isc.testclient.euraka.server;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
